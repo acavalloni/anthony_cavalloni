1ce8d554ed0b45a2c21e2dc1561a27a243be6d40
Anthony Cavalloni <anthonycavalloni@gmail.com>
2026-04-20 15:38:49 +0200

    clean and lighter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,125 +1,59 @@
 (function($){
-    
+
     "use strict";
-    
-    //===== Prealoder
-    
-    $(window).on('load', function(event) {
-        $('.preloader').delay(500).fadeOut(500);
-    });
-    
-    
-    //===== Mobile Menu 
-    
+
+    //===== Mobile Menu
+
     $(".navbar-toggler").on('click', function() {
         $(this).toggleClass('active');
     });
-    
+
     $(".navbar-nav a").on('click', function() {
         $(".navbar-toggler").removeClass('active');
-    });
-    
-    
-    //===== close navbar-collapse when a  clicked
-    
-    $(".navbar-nav a").on('click', function () {
         $(".navbar-collapse").removeClass("show");
     });
-    
-    
-    //===== Sticky
-    
-    $(window).on('scroll', function(event) {    
-        var scroll = $(window).scrollTop();
-        if (scroll < 10) {
+
+
+    //===== Sticky navigation
+
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() < 10) {
             $(".navigation").removeClass("sticky");
-        } else{
+        } else {
             $(".navigation").addClass("sticky");
         }
     });
-    
-    
-    //===== Section Menu Active
+
+
+    //===== Active section link on scroll
 
     var scrollLink = $('.page-scroll');
-        // Active link switching
-        $(window).scroll(function() {
+    $(window).scroll(function() {
         var scrollbarLocation = $(this).scrollTop();
-
         scrollLink.each(function() {
-
-          var sectionOffset = $(this.hash).offset().top - 73;
-
-          if ( sectionOffset <= scrollbarLocation ) {
-            $(this).parent().addClass('active');
-            $(this).parent().siblings().removeClass('active');
-          }
+            var sectionOffset = $(this.hash).offset().top - 73;
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
         });
     });
-    
-    
-    
-    // Parallaxmouse js
-    
-    function parallaxMouse() {
-        if ($('#parallax').length) {
-            var scene = document.getElementById('parallax');
-            var parallax = new Parallax(scene);
-        };
-    };
-    parallaxMouse();
-    
-    
-    //===== Progress Bar
-    
-    if($('.progress-line').length){
-        $('.progress-line').appear(function(){
-            var el = $(this);
-            var percent = el.data('width');
-            $(el).css('width',percent+'%');
-        },{accY: 0});
-    }
-    
-    
-    //===== Counter Up
-    
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1600,
-    });
-    
-    
-    //===== Magnific Popup
-    
-    $('.image-popup').magnificPopup({
-      type: 'image'
-    });
 
-	$('.text-popup').magnificPopup({
-		type: 'inline',
-		midClick: true,
-	});
-    
-    
+
+    //===== Magnific Popup
+
+    $('.image-popup').magnificPopup({type: 'image'});
+    $('.text-popup').magnificPopup({type: 'inline', midClick: true});
+
+
     //===== Back to top
-    
-    // Show or hide the sticky footer button
-    $(window).on('scroll', function(event) {
-        if($(this).scrollTop() > 600){
-            $('.back-to-top').fadeIn(200)
-        } else{
-            $('.back-to-top').fadeOut(200)
+
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 600) {
+            $('.back-to-top').fadeIn(200);
+        } else {
+            $('.back-to-top').fadeOut(200);
         }
-    });
-    
-    
-    //Animate the scroll to yop
-    $('.back-to-top').on('click', function(event) {
-        event.preventDefault();
-        
-        $('html, body').animate({
-            scrollTop: 0,
-        }, 1500);
     });
 
 }(jQuery));
